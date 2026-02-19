544dd070f67cce57c3aebefd123fbaa2141f4fd5
Steven Spray <fnanfne@gmail.com>
2026-02-19 16:17:35 +0000

    SETUP: Initial config for a Windows build
    Replaced CMakeLists.txt entirely, optimised for a Windows Build. Added some additional includes in rapt_CPU.h and changed the App name from 'RWK' to 'RWK_COM'.

--- RWK_Source/Framework/RAPT/rapt_CPU.h
+++ RWK_Source/Framework/RAPT/rapt_CPU.h
@@ -7,6 +7,16 @@
 #define CPUHOOKPTR std::function<void(CPU* theThis)>
 #define COPY(x) x=theSource->x
 
+// NEW COMMUNITY
+#ifndef RAPT_LEGACY //(NEW_COMMUNITY_2026)
+#define RAPT_LEGACY //(NEW_COMMUNITY_2026)
+#endif //(NEW_COMMUNITY_2026)
+
+#ifndef LEGACY_GL //(NEW_COMMUNITY_2026)
+#define LEGACY_GL //(NEW_COMMUNITY_2026)
+#endif //(NEW_COMMUNITY_2026)
+// NEW COMMUNITY
+
 //
 // The CPU class replaces the Gadget class from the old framework.
 // A CPU is an object that is intended to act as a "program" in its 
